f884eca14a0d6f8ab4be4dde802ad4b2c4f9e639
Raki <rakeshjagadeswaran@gmail.com>
2023-11-08 12:47:15 +0530

    js for script

---
+++ script.js
@@ -0,0 +1,18 @@
+const showModalButton = document.getElementById('show-modal');
+const modal = document.getElementById('modal');
+const closeModalButton = document.getElementById('close-modal');
+
+showModalButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Close the modal when clicking outside the modal content
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
